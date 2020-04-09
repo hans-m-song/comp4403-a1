539d95e53a265a01abde00de844cf04fb3c56311
Hans Song <hans.m.song@gmail.com>
2020-04-09 11:26:18 +1000

    submission

---
+++ submit.sh
@@ -0,0 +1,6 @@
+cp src/parse/Parser.java submission
+cp src/interpreter/Interpreter.java submission
+cp src/tree/StaticChecker.java submission
+cp src/tree/StatementNode.java submission/
+cp src/tree/StatementVisitor.java submission/
+
